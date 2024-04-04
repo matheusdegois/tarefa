71f5967a2546f2834d50f490cd67b7cffe509411
matheusdegois <163362900+matheusdegois@users.noreply.github.com>
2024-04-03 21:21:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,63 +1,72 @@
 ''@import url( https://fonts.googleapis.com/css2?family=Poppins:wght@200;400&display-swap");
 
 *{
-box-sizing: border-box;
+ box-sizing: border-box;
 }
+
 body {
-background-color: #£5£5£5;
-color: #444;
-font-family: "Poppins', sans-serif;
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-height: 100vh;
-margin: 0;
+ background-color: #£5£5£5;
+ color: #444;
+ font-family: 'Poppins', sans-serif;
+ display: flex;
+ flex-direction: column;
+ align-items; center;
+ justify-content: center;
+ height: 100vh;
+ margin: 0;
 }
+
 h1 {
-color: rgb(179, 131, 226);
-font-size: 10rem;
-text-align: center;
-opacity: 0.4;
+ color: rgb(179, 131, 226);
+ font-size: 10rem;
+ text-align: center;
+ opacity: 0.4;
 }
-form {
-box-shadow: 0 4px 10px rgba(o, 0, 0, 0.1);
-max-width: 100%;
-width: 400px;
+
+ form {
+ box-shadow: 0 4px 10px rgba(o, 0, 0, 0.1);
+ max-width: 100%;
+ width: 400px;
 }
+
 .input {
-border: none;
-color: #444:
-font-size: 2rem;
-padding: 1rem 2rem;
-display: block;
-width: 100%;
+ border: none;
+ olor: #444:
+ font-size: 2rem;
+ padding: 1rem 2rem;
+ display: block;
+ width: 100%;
 }
+
 .input:placeholder {
-color: #d5d5d5;
+ color: #d5d5d5;
 }
+
 .input:focus {
-outine-color: rgb(1 79, 131, 226);
+ outine-color: rgb(1 79, 131, 226);
 }
+
 .todos {
-background-color: #fff;
-padding: 0;
-margin: 0;
-list-style-type: none;
+ background-color: #fff;
+ padding: 0;
+ margin: 0;
+ list-style-type: none;
 }
+
 .todos Ii {
-border-top: 1px solid #e5e5e5;
-cursor: pointer;
-font-size: 1.5rem;
-padding: 1rem 2rem;
+ border-top: 1px solid #e5e5e5;
+ cursor: pointer;
+ font-size: 1.5rem;
+ padding: 1rem 2rem:
 }
+
 .todos li.completed {
-color: #6b6b6;
-text-decoration: line-through;
+ color: #6b6b6;
+ text-decoration: line-through;
 }
 
 small {
-color: #b5b5b5:
-margin-top: 3rem;
-text-align: center;
+ color: #b5b5b5:
+ margin-top: 3rem;
+ text-align: center;
 }
